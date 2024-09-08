4328c285de90194629c5d89155540d1be46c912d
Mark Bishop <mbishop@duosecurity.com>
2024-09-07 20:57:04 -0400

    Add ARM architecture

--- flake.nix
+++ flake.nix
@@ -4,46 +4,50 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-colors.url = "github:misterio77/nix-colors";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     nix-colors,
+    flake-utils,
   }: let
-    system = "x86_64-linux";
-    fontsize = 18;
-    colors = nix-colors.colorSchemes.gruvbox-material-dark-hard;
-    templateFile = name: template: data:
-      pkgs.stdenv.mkDerivation {
-        name = "${name}";
+    systems = ["x86_64-linux" "armv7l-linux"];
+    build = system: let
+      fontsize = 18;
+      colors = nix-colors.colorSchemes.gruvbox-material-dark-hard;
+      templateFile = name: template: data:
+        pkgs.stdenv.mkDerivation {
+          name = "${name}";
 
-        nativeBuildInpts = [pkgs.mustache-go];
+          nativeBuildInpts = [pkgs.mustache-go];
 
-        # Pass Json as file to avoid escaping
-        passAsFile = ["jsonData"];
-        jsonData = builtins.toJSON data;
+          # Pass Json as file to avoid escaping
+          passAsFile = ["jsonData"];
+          jsonData = builtins.toJSON data;
 
-        # Disable phases which are not needed. In particular the unpackPhase will
-        # fail, if no src attribute is set
-        phases = ["buildPhase" "installPhase"];
+          # Disable phases which are not needed. In particular the unpackPhase will
+          # fail, if no src attribute is set
+          phases = ["buildPhase" "installPhase"];
 
-        buildPhase = ''
-          ${pkgs.mustache-go}/bin/mustache $jsonDataPath ${template} > rendered_file
-        '';
+          buildPhase = ''
+            ${pkgs.mustache-go}/bin/mustache $jsonDataPath ${template} > rendered_file
+          '';
 
-        installPhase = ''
-          cp rendered_file $out
-        '';
+          installPhase = ''
+            cp rendered_file $out
+          '';
+        };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+        ];
       };
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [
-      ];
-    };
-  in {
-    packages = {
-      x86_64-linux = rec {
+    in {
+      packages = rec {
         st = pkgs.st.overrideAttrs (oldAttrs: rec {
           patches = [
           ];
@@ -57,5 +61,6 @@
         default = st;
       };
     };
-  };
+  in
+    flake-utils.lib.eachSystem systems build;
 }
